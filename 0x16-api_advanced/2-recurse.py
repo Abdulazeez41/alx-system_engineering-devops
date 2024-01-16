f428bd097c778e53db2a4cd143f26332d96ed578
Abdulazeez41 <abdulazeezsalihu41@gmail.com>
2024-01-17 00:52:29 +0100

    Added Function to query a list of all hot posts on a given Reddit subreddit

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Function to query a list of all hot posts on a given Reddit subreddit."""
+import requests
+
+
+def recurse(subreddit, hot_list=[], after="", count=0):
+    """
+    Returns a list of titles of all hot posts on a given subreddit.
+
+    :param subreddit: The name of the subreddit to query.
+    :param hot_list: A list to store the titles of hot posts.
+    :param after: Token for pagination to retrieve the next set of results.
+    :param count: The total count of posts retrieved.
+    :return: List of titles of all hot posts.
+    """
+    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    params = {
+        "after": after,
+        "count": count,
+        "limit": 100
+    }
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code == 404:
+        return None
+
+    results = response.json().get("data")
+    after = results.get("after")
+    count += results.get("dist")
+    for c in results.get("children"):
+        hot_list.append(c.get("data").get("title"))
+
+    if after is not None:
+        return recurse(subreddit, hot_list, after, count)
+    return hot_list 
